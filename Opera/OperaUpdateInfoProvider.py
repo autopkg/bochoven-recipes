599ccd85602168998f5945933627534bf661670f
Arjen van Bochoven <arjen@bochoven.net>
2014-10-21 22:05:41 +0200

    Update OperaUpdateInfoProvider.py
    The xml request was broken, but there is now a json rest api

--- Opera/OperaUpdateInfoProvider.py
+++ Opera/OperaUpdateInfoProvider.py
@@ -19,26 +19,15 @@ import urllib
 import urllib2
 import urlparse
 import os
-from xml.etree import ElementTree
+import json
 
 from autopkglib import Processor, ProcessorError
-from distutils.version import LooseVersion
-from operator import itemgetter
 
 __all__ = ["OperaUpdateInfoProvider"]
 
 class OperaUpdateInfoProvider(Processor):
-    description = "Provides URL to the highest version number or latest update."
-    input_variables = {
-        "update_url": {
-            "required": True,
-            "description": "URL for Opera update feed xml.",
-        },
-        "request_headers": {
-            "required": True,
-            "description": "Dictionary of additional HTTP headers to include in request.",
-        }
-    }
+    description = "Provides URL to the latest stable version."
+    input_variables = {}
     output_variables = {
         "url": {
             "description": "URL for a download.",
@@ -48,49 +37,20 @@ class OperaUpdateInfoProvider(Processor):
         },
         "filename": {
             "description": "Filename for the zip file.",
-        },
-        "archive_format": {
-            "description": "Zip archive.",
-       }
-
+        }
     }
 
     __doc__ = description
 
     def get_feed_data(self, url):
-        """Returns an array of dicts, with the primary and the fallback url:
-            version: Version number
-            tag: primary or fallback (not used yet)
+        """Returns a dict containing:
+            version: latest
             url: Url for the zip archive
         """
-        
 
-        # xml request that has to be sent to the opera update server
-        xml_req = """<?xml version="1.0"?>
-        <autoupdate schema-version="2.1" type="automatic">
-            <system>
-                <platform>
-                    <opsys>MacOS</opsys>
-                    <opsys-version>10.6.0</opsys-version>
-                    <arch>i386</arch>
-                    <package>ZIP</package>
-                </platform>
-            </system>
-            <product>
-                <name>Opera</name>
-                <version>1.0</version>
-                <language>en-US</language>
-                <edition/>
-            </product>
-        </autoupdate>"""
+        url = "https://autoupdate.geo.opera.com/netinstaller/Stable/MacOS"
 
-        request = urllib2.Request(url, xml_req)
-
-        # request header code borrowed from URLDownloader
-        if "request_headers" in self.env:
-            headers = self.env["request_headers"]
-            for header, value in headers.items():
-                request.add_header(header, value)
+        request = urllib2.Request(url)
 
         try:
             url_handle = urllib2.urlopen(request)
@@ -100,40 +60,25 @@ class OperaUpdateInfoProvider(Processor):
         print request.get_full_url()
         data = url_handle.read()
 
-        try:
-            xmldata = ElementTree.fromstring(data)
-        except:
-            raise ProcessorError("Error parsing XML from Opera Update feed.")
+        # Todo, add a try catch block
+        jsonData = json.loads(data)
+        
+        item = {}
+        item["version"] = 'latest'
+        item["filename"] = jsonData['installer_filename']
+        item["url"] = jsonData['installer']
 
-        items = xmldata.findall(".//primary")
-        version = xmldata.find(".//version").text
-
-        versions = []
-        for item_elem in items:
-
-            item = {}
-            item["version"] = version
-            item["tag"] = item_elem.tag
-            item["url"] = item_elem.text
-
-            versions.append(item)
-
-        return versions
+        return item
 
     def main(self):
-        def compare_version(a, b):
-            return cmp(LooseVersion(a), LooseVersion(b))
 
-        items = self.get_feed_data(self.env.get("update_url"))
-        sorted_items = sorted(items,
-                              key=itemgetter("version"),
-                              cmp=compare_version)
-        latest = sorted_items[-1]
+        latest = self.get_feed_data(self.env.get("update_url"))
+
         self.output("Version retrieved from opera xml: %s" % latest["version"])
 
         self.env["VERSION"] = latest["version"]
         self.env["url"] = latest["url"]
-        self.env["filename"] = "Opera-%s.zip" % latest["version"]
+        self.env["filename"] = latest["filename"]
         self.output("Found URL %s" % self.env["url"])
         
 
